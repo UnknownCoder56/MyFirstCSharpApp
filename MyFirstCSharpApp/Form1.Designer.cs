480f7f822d3517a194115b8e1330ee2b4f6dc622
arpan <arpan@ARPAN-HP-G42>
2021-08-03 17:29:43 +0530

    Add project files.

---
+++ MyFirstCSharpApp/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿
+namespace MyFirstCSharpApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxNumber1 = new System.Windows.Forms.TextBox();
+            this.textBoxNumber2 = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonSubtract = new System.Windows.Forms.Button();
+            this.buttonMultiply = new System.Windows.Forms.Button();
+            this.buttonDivide = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonRoot = new System.Windows.Forms.Button();
+            this.buttonPower = new System.Windows.Forms.Button();
+            this.buttonMod = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelAnswer = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Number 2";
+            // 
+            // textBoxNumber1
+            // 
+            this.textBoxNumber1.Location = new System.Drawing.Point(74, 54);
+            this.textBoxNumber1.Name = "textBoxNumber1";
+            this.textBoxNumber1.Size = new System.Drawing.Size(175, 20);
+            this.textBoxNumber1.TabIndex = 3;
+            // 
+            // textBoxNumber2
+            // 
+            this.textBoxNumber2.Location = new System.Drawing.Point(74, 81);
+            this.textBoxNumber2.Name = "textBoxNumber2";
+            this.textBoxNumber2.Size = new System.Drawing.Size(175, 20);
+            this.textBoxNumber2.TabIndex = 4;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(12, 145);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 5;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonSubtract
+            // 
+            this.buttonSubtract.Location = new System.Drawing.Point(93, 145);
+            this.buttonSubtract.Name = "buttonSubtract";
+            this.buttonSubtract.Size = new System.Drawing.Size(75, 23);
+            this.buttonSubtract.TabIndex = 6;
+            this.buttonSubtract.Text = "Subtract";
+            this.buttonSubtract.UseVisualStyleBackColor = true;
+            this.buttonSubtract.Click += new System.EventHandler(this.buttonSubtract_Click);
+            // 
+            // buttonMultiply
+            // 
+            this.buttonMultiply.Location = new System.Drawing.Point(174, 145);
+            this.buttonMultiply.Name = "buttonMultiply";
+            this.buttonMultiply.Size = new System.Drawing.Size(75, 23);
+            this.buttonMultiply.TabIndex = 7;
+            this.buttonMultiply.Text = "Multiply";
+            this.buttonMultiply.UseVisualStyleBackColor = true;
+            this.buttonMultiply.Click += new System.EventHandler(this.buttonMultiply_Click);
+            // 
+            // buttonDivide
+            // 
+            this.buttonDivide.Location = new System.Drawing.Point(12, 174);
+            this.buttonDivide.Name = "buttonDivide";
+            this.buttonDivide.Size = new System.Drawing.Size(75, 23);
+            this.buttonDivide.TabIndex = 8;
+            this.buttonDivide.Text = "Divide";
+            this.buttonDivide.UseVisualStyleBackColor = true;
+            this.buttonDivide.Click += new System.EventHandler(this.buttonDivide_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(93, 203);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 9;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonRoot
+            // 
+            this.buttonRoot.Location = new System.Drawing.Point(93, 174);
+            this.buttonRoot.Name = "buttonRoot";
+            this.buttonRoot.Size = new System.Drawing.Size(75, 23);
+            this.buttonRoot.TabIndex = 11;
+            this.buttonRoot.Text = "Root";
+            this.buttonRoot.UseVisualStyleBackColor = true;
+            this.buttonRoot.Click += new System.EventHandler(this.buttonRoot_Click);
+            // 
+            // buttonPower
+            // 
+            this.buttonPower.Location = new System.Drawing.Point(174, 174);
+            this.buttonPower.Name = "buttonPower";
+            this.buttonPower.Size = new System.Drawing.Size(75, 23);
+            this.buttonPower.TabIndex = 12;
+            this.buttonPower.Text = "Power";
+            this.buttonPower.UseVisualStyleBackColor = true;
+            this.buttonPower.Click += new System.EventHandler(this.buttonPower_Click);
+            // 
+            // buttonMod
+            // 
+            this.buttonMod.Location = new System.Drawing.Point(12, 203);
+            this.buttonMod.Name = "buttonMod";
+            this.buttonMod.Size = new System.Drawing.Size(75, 23);
+            this.buttonMod.TabIndex = 13;
+            this.buttonMod.Text = "Mod";
+            this.buttonMod.UseVisualStyleBackColor = true;
+            this.buttonMod.Click += new System.EventHandler(this.buttonMod_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Answer";
+            // 
+            // labelAnswer
+            // 
+            this.labelAnswer.AutoSize = true;
+            this.labelAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAnswer.Location = new System.Drawing.Point(74, 108);
+            this.labelAnswer.Name = "labelAnswer";
+            this.labelAnswer.Size = new System.Drawing.Size(14, 13);
+            this.labelAnswer.TabIndex = 15;
+            this.labelAnswer.Text = "0";
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(174, 204);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 16;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 246);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.labelAnswer);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonMod);
+            this.Controls.Add(this.buttonPower);
+            this.Controls.Add(this.buttonRoot);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonDivide);
+            this.Controls.Add(this.buttonMultiply);
+            this.Controls.Add(this.buttonSubtract);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxNumber2);
+            this.Controls.Add(this.textBoxNumber1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxNumber1;
+        private System.Windows.Forms.TextBox textBoxNumber2;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonSubtract;
+        private System.Windows.Forms.Button buttonMultiply;
+        private System.Windows.Forms.Button buttonDivide;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonRoot;
+        private System.Windows.Forms.Button buttonPower;
+        private System.Windows.Forms.Button buttonMod;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelAnswer;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
---
+++ MyFirstCSharpApp/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyFirstCSharpApp
+{
+    public partial class Form1 : Form
+    {
+        public double num1, num2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                num1 = double.Parse(textBoxNumber1.Text);
+                num2 = double.Parse(textBoxNumber2.Text);
+                labelAnswer.Text = $"{num1 + num2}";
+            } catch { MessageBox.Show("Please enter numbers in both the text fields."); }
+        }
+
+        private void buttonSubtract_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                num1 = double.Parse(textBoxNumber1.Text);
+                num2 = double.Parse(textBoxNumber2.Text);
+                labelAnswer.Text = $"{num1 - num2}";
+            }
+            catch { MessageBox.Show("Please enter numbers in both the text fields."); }
+        }
+
+        private void buttonMultiply_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                num1 = double.Parse(textBoxNumber1.Text);
+                num2 = double.Parse(textBoxNumber2.Text);
+                labelAnswer.Text = $"{num1 * num2}";
+        } catch { MessageBox.Show("Please enter numbers in both the text fields."); }
+}
+
+        private void buttonDivide_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                num1 = double.Parse(textBoxNumber1.Text);
+                num2 = double.Parse(textBoxNumber2.Text);
+                labelAnswer.Text = $"{num1 / num2}";
+            } catch { MessageBox.Show("Please enter numbers in both the text fields."); }
+        }
+
+        private void buttonRoot_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                num1 = double.Parse(textBoxNumber1.Text);
+                num2 = double.Parse(textBoxNumber2.Text);
+                labelAnswer.Text = $"{Math.Sqrt(num1)}";
+            } catch { MessageBox.Show("Please enter numbers in both the text fields."); }
+        }
+
+        private void buttonPower_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                num1 = double.Parse(textBoxNumber1.Text);
+                num2 = double.Parse(textBoxNumber2.Text);
+                labelAnswer.Text = $"{Math.Pow(num1, num2)}";
+            } catch { MessageBox.Show("Please enter numbers in both the text fields."); }
+        }
+
+        private void buttonMod_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                num1 = double.Parse(textBoxNumber1.Text);
+                num2 = double.Parse(textBoxNumber2.Text);
+                labelAnswer.Text = $"{num1 % num2}";
+            } catch { MessageBox.Show("Please enter numbers in both the text fields."); }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxNumber1.Text = "";
+            textBoxNumber2.Text = "";
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
